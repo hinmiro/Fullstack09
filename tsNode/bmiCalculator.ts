1a1a50d68733b36956983ecad3a999da853c557e
mirohi <miro.hintikka@metropolia.fi>
2025-01-27 09:42:22 +0200

    Add assignment 9.1

--- tsNode/bmiCalculator.ts
+++ tsNode/bmiCalculator.ts
@@ -1,21 +1,20 @@
-type Value = 'Underweight' | 'Normal' | 'Obese' | 'Extremely obese'
-
-const calculateBmi = (length: number, weight: number): Value => {
+const calculateBmi = (length: number, weight: number): string => {
     const bmi = parseFloat((weight / Math.pow(length / 100, 2)).toFixed(2))
     if (bmi <= 18) {
-        return 
+        return 'Underweight range'
     }
-    if (bmi >= 18 && bmi <= 24) {
-        return 'Normal'
+    if (bmi > 18 && bmi <= 24) {
+        return 'Normal range'
     }
     if (bmi >= 25 && bmi <= 29) {
-        return 'Light overweight'
+        return 'Light overweight range'
     }
     if (bmi >= 30 && bmi <= 35) {
-        return 'Overweight'
+        return 'Overweight range'
     }
     if (bmi > 35) {
-        return 'Extreme overweight'
+        return 'Extreme overweight range'
     }
+    return 'Not valid inputs'
 }
-console.log(calculateBmi(172, 57))
+console.log(calculateBmi(180, 72))
