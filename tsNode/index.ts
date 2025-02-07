209890f92cd7106f312a350852660e79300c2974
mirohi <miro.hintikka@metropolia.fi>
2025-02-07 17:02:08 +0200

    Add assignment 9.7

--- tsNode/index.ts
+++ tsNode/index.ts
@@ -1,7 +1,9 @@
 import express, { Express, Response, Request } from 'express';
 import { calculateBmi } from './service/bmiCalculator';
+import { calculateExercises } from './service/exerciseCalculator';
 
 const app: Express = express();
+app.use(express.json());
 
 app.get('/hello', (_req: Request, res: Response): void => {
     res.send('Hello Full Stack!');
@@ -11,8 +13,8 @@ app.get('/bmi', (req: Request, res: Response): void => {
     try {
         const response = calculateBmi(parseFloat(<string>req.query.height), parseFloat(<string>req.query.weight));
         res.send(response);
-    } catch (err) {
-        res.status(400).json({ error: 'Malformed parameters', err});
+    } catch {
+        res.status(400).json({ error: 'Malformed parameters' });
     }
 });
 
@@ -20,4 +22,38 @@ const PORT: number = 3003;
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
+});
+
+app.post('/exercises', (req: Request, res: Response): void => {
+    try {
+        const { daily_exercises, target } = req.body as { daily_exercises: Array<number>, target: number };
+
+        if (!daily_exercises || !target) {
+            res.status(400).json({ error: 'Parameters missing' });
+        }
+
+        if (!Array.isArray(daily_exercises)) {
+            res.status(400).json({ error: 'Malformatted parameters' });
+        }
+
+        for (const e of daily_exercises) {
+            if (isNaN(e)) {
+                res.status(400).json({ error: 'Malformatted parameters' });
+            }
+        }
+
+        if (isNaN(target)) {
+            res.status(400).json({ error: 'Malformed parameters' });
+        }
+
+        const response = calculateExercises(daily_exercises, target);
+        console.log(response);
+        if ('error' in response) {
+            res.status(400).json({ error: 'Malformed parameters' });
+        }
+
+        res.send(response);
+    } catch {
+        res.status(500).json({ error: 'Bad server... Bad!' });
+    }
 });
--- tsNode/service/exerciseCalculator.ts
+++ tsNode/service/exerciseCalculator.ts
@@ -1,4 +1,4 @@
-type Objective = {
+type ExerciseJson = {
     periodLength: number,
     trainingDays: number,
     success: boolean,
@@ -8,33 +8,28 @@ type Objective = {
     average: number
 }
 
-const processArguments = (args: Array<string>): Array<number> => {
-    return args.map(a => {
-        const parsed = parseInt(a)
-        if (isNaN(parsed)) {
-            throw new Error('Invalid arguments')
-        }
-        return parsed
-    })
+type ErrorJson = {
+    error: string
 }
 
-const calculateExercises = (exercises: Array<number>, target: number): Objective => {
 
-    const trainingDays: number = exercises.filter(day => day > 0).length
-    const total: number = exercises.reduce((acc, curr) => acc + curr, 0)
-    const average: number = total / exercises.length
-    let rating: number
-    let ratingDesc: string
+export const calculateExercises = (exercises: Array<number>, target: number): ExerciseJson | ErrorJson => {
+
+    const trainingDays: number = exercises.filter(day => day > 0).length;
+    const total: number = exercises.reduce((acc, curr) => acc + curr, 0);
+    const average: number = total / exercises.length;
+    let rating: number;
+    let ratingDesc: string;
 
     if (average > 3 * trainingDays / exercises.length) {
-        rating = 3
-        ratingDesc = 'Excellent! Keep on good job.'
+        rating = 3;
+        ratingDesc = 'Excellent! Keep on good job.';
     } else if (average > 2 * trainingDays / exercises.length) {
-        rating = 2
-        ratingDesc = 'You almost did it! Keep on working.'
+        rating = 2;
+        ratingDesc = 'You almost did it! Keep on working.';
     } else {
-        rating = 1
-        ratingDesc = 'Needs bit more effort, dont you think?'
+        rating = 1;
+        ratingDesc = 'Needs bit more effort, dont you think?';
     }
 
     return {
@@ -45,16 +40,5 @@ const calculateExercises = (exercises: Array<number>, target: number): Objective
         ratingDescription: ratingDesc,
         target: target,
         average: average,
-    }
-}
-
-try {
-    const hours: Array<number> = processArguments(process.argv.slice(3))
-    console.log(calculateExercises(hours.slice(1), hours[0]))
-} catch (error: unknown) {
-    let errorMsg: string = 'Something went wrong'
-    if (error instanceof Error) {
-        errorMsg += ' Error: ' + error.message
-    }
-    console.log(errorMsg)
-}
+    };
+};
