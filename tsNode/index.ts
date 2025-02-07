c01d874e8048931d950bb5e44b52d9798efed8b4
mirohi <miro.hintikka@metropolia.fi>
2025-02-07 15:48:57 +0200

    Add assignment 9.6

--- tsNode/index.ts
+++ tsNode/index.ts
@@ -1,23 +1,23 @@
-import express, { Express, Response, Request } from 'express'
-import { calculateBmi } from './service/bmiCalculator'
+import express, { Express, Response, Request } from 'express';
+import { calculateBmi } from './service/bmiCalculator';
 
-const app: Express = express()
+const app: Express = express();
 
-app.get('/hello', (req: Request, res: Response): void => {
-    res.send('Hello Full Stack!')
-})
+app.get('/hello', (_req: Request, res: Response): void => {
+    res.send('Hello Full Stack!');
+});
 
 app.get('/bmi', (req: Request, res: Response): void => {
     try {
-        const response = calculateBmi(parseFloat(<string>req.query.height), parseFloat(<string>req.query.weight))
-        res.send(response)
+        const response = calculateBmi(parseFloat(<string>req.query.height), parseFloat(<string>req.query.weight));
+        res.send(response);
     } catch (err) {
-        res.status(400).json({ error: 'Malformed parameters' })
+        res.status(400).json({ error: 'Malformed parameters', err});
     }
-})
+});
 
-const PORT: number = 3003
+const PORT: number = 3003;
 
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`)
-})
+    console.log(`Server running on http://localhost:${PORT}`);
+});
--- tsNode/service/bmiCalculator.ts
+++ tsNode/service/bmiCalculator.ts
@@ -1,29 +1,29 @@
 type BmiObject = { weight: number, height: number, bmi: string }
 
 export const calculateBmi = (length: number, weight: number) => {
-    const bmi = parseFloat((weight / Math.pow(length / 100, 2)).toFixed(2))
-    let bmiResponse = {weight: weight, height: length, bmi: ''}
+    const bmi = parseFloat((weight / Math.pow(length / 100, 2)).toFixed(2));
+    const bmiResponse = { weight: weight, height: length, bmi: '' } as BmiObject;
 
     if (bmi <= 18) {
-        bmiResponse.bmi = 'Underweight range'
-        return bmiResponse
+        bmiResponse.bmi = 'Underweight range';
+        return bmiResponse;
     }
     if (bmi > 18 && bmi < 25) {
-        bmiResponse.bmi = 'Normal range'
-        return bmiResponse
+        bmiResponse.bmi = 'Normal range';
+        return bmiResponse;
     }
     if (bmi >= 25 && bmi < 30) {
-        bmiResponse.bmi = 'Light overweight range'
-        return bmiResponse
+        bmiResponse.bmi = 'Light overweight range';
+        return bmiResponse;
     }
     if (bmi > 30 && bmi <= 35) {
-        bmiResponse.bmi = 'Overweight range'
-        return bmiResponse
+        bmiResponse.bmi = 'Overweight range';
+        return bmiResponse;
     }
     if (bmi > 35) {
-        bmiResponse.bmi = 'Extreme overweight range'
-        return bmiResponse
+        bmiResponse.bmi = 'Extreme overweight range';
+        return bmiResponse;
     }
-    throw new Error('Bad parameters')
-}
+    throw new Error('Bad parameters');
+};
 
