cd5ab5f49850a224ffe5425e4edb19577ae7db55
mirohi <miro.hintikka@metropolia.fi>
2025-03-06 20:07:31 +0200

    Add assignment 9.17

--- flightDiaryFront/src/App.tsx
+++ flightDiaryFront/src/App.tsx
@@ -1,33 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+interface Entry {
+  id: number,
+  date: string,
+  weather: string,
+  visibility: string,
+  comment: string,
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [entries, setEntries] = useState<Entry[]>([])
+
+  useEffect(() => {
+    axios.get<Entry[]>('http://localhost:3000/api/diaries').then(response => {
+      console.log(response.data)
+      setEntries(response.data)
+    })
+  }, [])
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Flight diary</h1>
+      <table style={{flex: 1}}>
+        <tbody>
+        {entries.map((entry, i: number) => (
+          <tr key={i}>
+            <td><i>{entry.date}</i></td>
+            <td>{entry.weather}</td>
+            <td>{entry.visibility}</td>
+            <td style={{paddingLeft: '3rem'}}>{entry.comment}</td>
+          </tr>
+        ))}
+        </tbody>
+      </table>
     </>
   )
 }
