0433d5209e8d3eca0bd3887500bfbd75af45d056
hinmiro <miro.hintikka@gmail.com>
2025-06-11 20:30:08 +0300

    Add assignment 9.20

--- flightDiaryFront/src/App.tsx
+++ flightDiaryFront/src/App.tsx
@@ -11,6 +11,13 @@ function App() {
   const [visibility, setVisibility] = useState('')
   const [comment, setComment] = useState('')
   const [errorMessage, setErrorMessage] = useState('')
+  const [date, setDate] = useState(() => {
+    const today = new Date()
+    const yyyy = today.getFullYear()
+    const mm = String(today.getMonth() + 1).padStart(2, '0')
+    const dd = String(today.getDate()).padStart(2, '0')
+    return `${yyyy}-${mm}-${dd}`
+  })
 
   useEffect(() => {
     axios.get<Entry[]>('http://localhost:3000/api/diaries').then((response) => {
@@ -21,9 +28,8 @@ function App() {
 
   const addEntry = (event: React.SyntheticEvent) => {
     event.preventDefault()
-    const date = new Date().toISOString().toString().split('T')
     const entry: Entry = {
-      date: date[0].toString(),
+      date: date.toString(),
       weather: weather,
       visibility: visibility,
       comment: comment,
@@ -55,20 +61,104 @@ function App() {
         <form onSubmit={addEntry}>
           <div>
             <div style={{ display: 'flex', gap: '1rem' }}>
+              <label>Date:</label>
+              <input
+                type="date"
+                aria-label={'Date:'}
+                value={date}
+                onChange={(event) => setDate(event.target.value)}
+              />
+            </div>
+            <div style={{ display: 'flex', gap: '1rem' }}>
               <label>Weather:</label>
               <input
-                aria-label={'Weather: '}
-                value={weather}
+                type="radio"
+                id="sunny"
+                name="weather"
+                value="sunny"
+                aria-label="sunny"
                 onChange={(event) => setWeather(event.target.value)}
               />
+              <label htmlFor="sunny">Sunny</label>
+
+              <input
+                type="radio"
+                id="rainy"
+                name="weather"
+                value="rainy"
+                onChange={(event) => setWeather(event.target.value)}
+              />
+              <label htmlFor="rainy">Rainy</label>
+
+              <input
+                type="radio"
+                id="cloudy"
+                name="weather"
+                value="cloudy"
+                onChange={(event) => setWeather(event.target.value)}
+              />
+              <label htmlFor="cloudy">Cloudy</label>
+
+              <input
+                type="radio"
+                id="stormy"
+                name="weather"
+                value="stormy"
+                onChange={(event) => setWeather(event.target.value)}
+              />
+              <label htmlFor="stormy">Stormy</label>
+
+              <input
+                type="radio"
+                id="windy"
+                name="weather"
+                value="windy"
+                onChange={(event) => setWeather(event.target.value)}
+              />
+              <label htmlFor="windy">Windy</label>
             </div>
             <div style={{ display: 'flex', gap: '1rem' }}>
               <label>Visibility:</label>
               <input
-                aria-label="Visibility: "
-                value={visibility}
+                type="radio"
+                id="great"
+                name="visibility"
+                value="great"
+                aria-label="great"
                 onChange={(event) => setVisibility(event.target.value)}
               />
+              <label htmlFor="great">Great</label>
+
+              <input
+                type="radio"
+                id="good"
+                name="visibility"
+                value="good"
+                aria-label="good"
+                onChange={(event) => setVisibility(event.target.value)}
+              />
+              <label htmlFor="good">Good</label>
+
+              <input
+                type="radio"
+                id="ok"
+                name="visibility"
+                value="ok"
+                aria-label="ok"
+                onChange={(event) => setVisibility(event.target.value)}
+              />
+              <label htmlFor="ok">Ok</label>
+
+              <input
+                type="radio"
+                id="poor"
+                name="visibility"
+                value="poor"
+                aria-label="poor"
+                onChange={(event) => setVisibility(event.target.value)}
+              />
+              <label htmlFor="poor">Poor</label>
+
             </div>
             <div style={{ display: 'flex', gap: '1rem' }}>
               <label>Comment:</label>
