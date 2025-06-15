58fc69d0a361b73e7705174bb9431c029ecca7c8
hinmiro <miro.hintikka@gmail.com>
2025-06-15 12:18:44 +0300

    Add assignment 9.22 (not ready)

---
+++ patientor_front/src/components/PatientPage/PatientDetails.tsx
@@ -0,0 +1,15 @@
+import { Patient } from '../../types';
+
+interface Props {
+  patients: Patient[];
+}
+
+export const PatientDetails = ({ patients }: Props) => {
+  return (
+    <>
+      <div>
+        <p>TOIMII TÄNNE</p>
+      </div>
+    </>
+  );
+};
